86321bf30afef1a3533faa4c99e0e28b3b4f5488
Çağatay <103611008+NarielV@users.noreply.github.com>
2023-12-30 03:41:42 +0300

    v2

--- WebProject/ViewModels/SelectSeat.cs
+++ WebProject/ViewModels/SelectSeat.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebProject.Models;
 
 namespace WebProject.ViewModels
 {
@@ -9,7 +10,7 @@ namespace WebProject.ViewModels
     {
         public int ShowtimeId { get; set; }
         public int TheaterId { get; set; }
-        public TheaterLayout TheaterLayout { get; set; }
+        public TheaterLayouts TheaterLayout { get; set; }
         public Dictionary<int, int> TicketQuantities { get; set; } // TicketTypeId and Quantity
                                                                    // Add any other properties needed for seat selection
     }
