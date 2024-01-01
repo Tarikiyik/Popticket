edfe8683fea01a5e1f26f49550f29cb39da2f39d
Çağatay <103611008+NarielV@users.noreply.github.com>
2024-01-02 02:28:57 +0300

    script

--- WebProject/Controllers/HomeController.cs
+++ WebProject/Controllers/HomeController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebProject.Models;
 
 
 namespace WebProject.Controllers
 {
     public class HomeController : Controller
     {
+        private PopTicketEntities db = new PopTicketEntities();
         public ActionResult Homepage()
         {
             return View();
--- WebProject/Scripts/seatselect.js
+++ WebProject/Scripts/seatselect.js
@@ -4,35 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const totalPrice = window.showtimeData.totalPrice;
     const continueButton = document.getElementById('continue-button');
 
-    let countdownDisplay = document.getElementById('countdown-timer'); // Add an element with this ID to your HTML where you want to show the timer
-    let countdownTime = 5 * 60 * 1000; // 5 minutes in milliseconds
-    let alertShown = false; // Flag to track if alert has been shown
-
-    // Function to update countdown display
-    function updateCountdownDisplay(timeLeft) {
-        let minutes = Math.floor(timeLeft / 60000);
-        let seconds = ((timeLeft % 60000) / 1000).toFixed(0);
-        countdownDisplay.textContent = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-    }
-
-    // Set a timer for 5 minutes
-    let timer = setInterval(function () {
-        countdownTime -= 1000; // Decrease time by one second
-        updateCountdownDisplay(countdownTime);
-
-        if (countdownTime <= 0) {
-            clearInterval(timer); // Stop the timer
-
-            if (!alertShown) {
-                alert("Time's up! Redirecting to another page.");
-                alertShown = true;
-                navigator.sendBeacon('/BuyTicket/ClearPendingReservations');
-                window.location.href = 'Movies/OnTheaters';
-            }
-        }
-    }, 1000);
-
-
     // Select or Deselect Seat
     function selectSeat(seatId) {
         const seatElement = document.querySelector(`[data-seat-id='${seatId}']`);
