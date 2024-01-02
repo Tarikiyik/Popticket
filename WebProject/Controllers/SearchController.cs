b9326b30e2b570ddc537427e88313f87f2292230
Çağatay <103611008+NarielV@users.noreply.github.com>
2024-01-02 20:39:06 +0300

    search

---
+++ WebProject/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Web;
+using System.Net;
+using System.Transactions;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Newtonsoft.Json;
+using WebProject.Models;
+using WebProject.ViewModels;
+
+namespace WebProject.Controllers
+{
+    public class SearchController : Controller
+    {
+        private PopTicketEntities db = new PopTicketEntities();
+
+        [HttpGet]
+        public ActionResult Results(string query)
+        {
+            var searchResults = db.Movie
+                .Where(m => m.title.Contains(query))
+                .ToList();
+
+            // Assume you have a SearchResults ViewModel
+            var viewModel = new SearchResults
+            {
+                Query = query,
+                Movies = searchResults
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebProject/ViewModels/SearchResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebProject.Models;
+
+namespace WebProject.ViewModels
+{
+    public class SearchResults
+    {
+        public string Query { get; set; }
+        public List<Movie> Movies { get; set; }
+    }
+}
